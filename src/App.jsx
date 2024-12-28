5719a77af4e5364d1a0ffaff58232ec06270f86e
RanemElnoury <120828846+RanemElnoury@users.noreply.github.com>
2024-12-28 22:12:31 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
+import React from "react";
+import { HashRouter as Router } from "react-router-dom";
 import Navbar from "./shared/navbar/Navbar.jsx";
 import Footer from "./shared/footer/Footer.jsx";
 import RoutesConfig from "./routes/RoutesConfig.jsx";
-import { HashRouter as Router } from "react-router-dom";
 import "./styles/variables.css";
 import "./styles/index.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Router>
+        <Navbar />
         <RoutesConfig />
+        <Footer />
       </Router>
-      <Footer />
     </div>
   );
 }
