7f226d1d81e6a60cf93c5dff681d1664f93599f6
RanemElnoury <120828846+RanemElnoury@users.noreply.github.com>
2024-12-28 23:20:53 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import React from "react";
 import { HashRouter as Router } from "react-router-dom";
 import Navbar from "./shared/navbar/Navbar.jsx";
 import Footer from "./shared/footer/Footer.jsx";
 import RoutesConfig from "./routes/RoutesConfig.jsx";
+import "./styles/variables.css";
+import "./styles/index.css";
 
 function App() {
+  const basename = process.env.NODE_ENV === "production" ? "/Hotels_" : "";
   return (
-    <Router >
-      <Navbar />
-      <RoutesConfig />
-      <Footer />
-    </Router>
+    <div className="App">
+      
+      <Router basename={basename}>
+        <Navbar />
+        <RoutesConfig />
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
